73ae9fbd43a39a4a6582637893812fba99989e96
David Sheehy <ds@schwey.io>
2015-06-08 23:01:38 -0500

    Added common colors to the config file.

--- juggling/config.py
+++ juggling/config.py
@@ -3,3 +3,13 @@ Various contants and sizes which are used throughout this project.
 """
 
 WINDOW_SIZE = WIDTH, HEIGHT =  600, 480
+
+
+#color
+WHITE  = (255, 255, 255)
+RED    = (255,   0,   0)
+GREEN  = (0  , 255,   0)
+BLUE   = (0  ,   0, 255)
+PURPLE = (255,   0, 255)
+YELLOW = (255, 255,   0) 
+CYAN   = (  0, 255, 255)
